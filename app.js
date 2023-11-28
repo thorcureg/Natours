97b1aa3f400c3c0eb2979e2bf7e5be082d1ea1cd
HectorCureg <thorcureg@gmail.com>
2023-11-28 14:01:46 +0800

    Section 8-Createing a Simple Tour Model

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 const express = require('express');
 const morgan = require('morgan')
@@ -22,3 +23,36 @@ app.use('/api/v1/users', userRouter);
 //4)start server
 
 module.exports = app;
+=======
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
+>>>>>>> 21a3c76 (Section 8-Createing a Simple Tour Model)
