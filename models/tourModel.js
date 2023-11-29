54c112ae25b1d06853b0fe26ea38172ace0bc3e8
hector <hector@bluehive.com.ph>
2023-11-29 16:51:37 +0800

    Section 8:95 2:14

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must hae a name'],
         unique: true,
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5,
+        required: [true, 'A tour must hae a rating'],
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
         required: [true, 'A tour must hae a rating'],
     },
     price: {
         type: Number,
         required: [true, 'A tour must hae a price'],
     },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description'],
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    startDate: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
